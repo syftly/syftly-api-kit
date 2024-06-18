b7d2d8f06ea17f3355a9ca080b54ce5d7b212f8a
willmorris44 <will.morris@syftly.com>
2024-06-18 12:20:20 -0600

    Initial

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "syftly-api-kit",
+    products: [.library(name: "SyftlyKit", targets: ["syftly-api-kit"])],
+    dependencies: [
+        .package(url: "https://github.com/binarybirds/swift-html", from: "1.7.0"),
+    ],
+    targets: [
+        .target(
+            name: "syftly-api-kit",
+            dependencies: [
+                .product(name: "SwiftHtml", package: "swift-html")
+            ]
+        )
+    ]
+)
---
+++ Sources/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Will Morris on 6/18/24.
+//
+
+import Foundation
